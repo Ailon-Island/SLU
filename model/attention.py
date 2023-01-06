5a1c2300a9311ccbbd21a8be5f48a892b1b81382
Flick <79459207+flick-ai@users.noreply.github.com>
2023-01-07 00:07:24 +0800

    delete

--- model/attention.py
+++ model/attention.py
@@ -3,35 +3,22 @@ import torch
 import torch.nn.functional as F
 import math
 
+
 class QKVAttention(nn.Module):
     def __init__(self, query_dim, key_dim, value_dim, hidden_dim, output_dim, dropout_rate):
         super(QKVAttention, self).__init__()
-
-        # Record hyper-parameters.
         self.__query_dim = query_dim
         self.__key_dim = key_dim
         self.__value_dim = value_dim
         self.__hidden_dim = hidden_dim
         self.__output_dim = output_dim
         self.__dropout_rate = dropout_rate
-
-        # Declare network structures.
         self.__query_layer = nn.Linear(self.__query_dim, self.__hidden_dim)
         self.__key_layer = nn.Linear(self.__key_dim, self.__hidden_dim)
         self.__value_layer = nn.Linear(self.__value_dim, self.__output_dim)
         self.__dropout_layer = nn.Dropout(p=self.__dropout_rate)
 
     def forward(self, input_query, input_key, input_value):
-        """ The forward propagation of attention.
-        Here we require the first dimension of input key
-        and value are equal.
-        :param input_query: is query tensor, (n, d_q)
-        :param input_key:  is key tensor, (m, d_k)
-        :param input_value:  is value tensor, (m, d_v)
-        :return: attention based tensor, (n, d_h)
-        """
-
-        # Linear transform to fine-tune dimension.
         linear_query = self.__query_layer(input_query)
         linear_key = self.__key_layer(input_key)
         linear_value = self.__value_layer(input_value)
@@ -42,22 +29,16 @@ class QKVAttention(nn.Module):
         ) / math.sqrt(self.__hidden_dim), dim=-1)
         forced_tensor = torch.matmul(score_tensor, linear_value)
         forced_tensor = self.__dropout_layer(forced_tensor)
-
         return forced_tensor
 
 
 class SelfAttention(nn.Module):
-
     def __init__(self, input_dim, hidden_dim, output_dim, dropout_rate):
         super(SelfAttention, self).__init__()
-
-        # Record parameters.
         self.__input_dim = input_dim
         self.__hidden_dim = hidden_dim
         self.__output_dim = output_dim
         self.__dropout_rate = dropout_rate
-
-        # Record network parameters.
         self.__dropout_layer = nn.Dropout(self.__dropout_rate)
         self.__attention_layer = QKVAttention(
             self.__input_dim, self.__input_dim, self.__input_dim,
@@ -65,8 +46,6 @@ class SelfAttention(nn.Module):
         )
 
     def forward(self, input_x, seq_lens, is_flat=False):
-        # import ipdb
-        # ipdb.set_trace()
         dropout_x = self.__dropout_layer(input_x)
         attention_x = self.__attention_layer(
             dropout_x, dropout_x, dropout_x
